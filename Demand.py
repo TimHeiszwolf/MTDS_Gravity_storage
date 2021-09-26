e65c8d36d85c4ccacf42bcc68f5ecffbf4045047
Tim Heiszwolf <32781879+TimHeiszwolf@users.noreply.github.com>
2021-09-27 01:29:27 +0200

    ADD make_3Dfunction_plot
    Tried (and failed) to quickly make a quick and dirty plotter for the demand data.

--- Demand.py
+++ Demand.py
@@ -3,12 +3,11 @@ import numpy as np
 import datetime
 import math
 
-
 class Households:
     """
     The households class is an object to store the data from the imported CSV file, then ready it for the modeling and finally giving values (with interpolation) to the simulation.
     """
-    def __init__(self, amount_of_households_per_type = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], power_consumption_per_type = [3500, 1000, 5000, 6900, 4200, 1, 0, 20000, 13000, 4000], profiel = "profielen Elektriciteit 2019 versie 1.00.csv"):
+    def __init__(self, amount_of_households_per_type = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0], power_consumption_per_type = [3500, 1000, 5000, 6900, 4200, 1, 0, 20000, 13000, 4000], profiel = "profielen Elektriciteit 2019 versie 1.00.csv"):
         """
         The initialize fuction for the households class imports the data from a CSV file and then readies it by cleaning up the data and coverting it into actual power use.
         
@@ -57,8 +56,17 @@ class Households:
         estimated_row_bot = math.floor(time_seconds / (15 * 60) + time_days * 24 * 4)# Makes a estimate for which row the data is in we are searching for.
         wanted_time = time_seconds + time_days * 3600 * 24
         
+        # print("Getting consumption at estimated index", estimated_row_bot)
         if not (self.data["Seconds"][estimated_row_bot] <= wanted_time and self.data["Seconds"][estimated_row_bot + 1] >= wanted_time):# Checks if the estimate row is correct.
-            print("Attention for time", wanted_time, "(s) the rows are not predicatable anymore.")
+            #print("Attention for time", wanted_time / (3600 * 24), "(d) the rows are not predicatable anymore.")
+            
+            if self.data["Seconds"][estimated_row_bot] > wanted_time:# If estimate is incorrect determine if to high or to low.
+                direction = -1
+            else:
+                direction = 1
+            
+            while not (self.data["Seconds"][estimated_row_bot] <= wanted_time and self.data["Seconds"][estimated_row_bot + 1] >= wanted_time):# Until the correct row is found go lower or higher.
+                estimated_row_bot = estimated_row_bot + direction
         
         interpolation_factor = (wanted_time - self.data["Seconds"][estimated_row_bot]) / (self.data["Seconds"][estimated_row_bot + 1] - self.data["Seconds"][estimated_row_bot]) # time_difference_for_bot_point / time_difference_between_points
         
@@ -73,10 +81,62 @@ class Households:
         else:
             return results
 
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib import pyplot
+from matplotlib import cm
+
+def make_3Dfunction_plot(function, amount_of_days = 364, increments_in_day = 200):
+    """
+    Makes a 3D plot like in the paper of Elke Klaasen (on the X axis the hours of the day, Y axis the days (of the year) and on the Z axis the actual value). Sadly makes a surface plot instead of a wireframe plot this is because the heatmap didn't work properly with the wireframe.
+    
+    function is the function of which it is going to make the wire plot
+    amount_of_days is the amount of days you want the plot to coverting
+    increments_in_day is the amount of steps during each day. Making this number large increases the x-axis accuracy of the plot but also increases computational time greatly.
+    """
+    
+    x = []
+    y = []
+    z = []
+    
+    x_new = []
+    y_new = []
+    z_new = []
+    
+    for day in range(0, amount_of_days):# For each individual day on the y-axis you make a new array containing the values for each hour (including the x and y-axis values themselfs) this you then append to the total array.
+        for hour in np.linspace(0, 24, increments_in_day, endpoint = True):
+            x_new.append(hour)
+            y_new.append(day)
+            z_new.append(function(hour * 3600, day))
+        
+        x.append(x_new)
+        y.append(y_new)
+        z.append(z_new)
+        
+        x_new = []
+        y_new = []
+        z_new = []
+    
+    x = np.array(x)
+    y = np.array(y)
+    z = np.array(z)
+    
+    #print(x, np.shape(x))
+    #print(y, np.shape(y))
+    #print(z, np.shape(y))
+    
+    fig = pyplot.figure(figsize=(8,8))
+    wf = fig.add_subplot(111, projection='3d')
+    #wf.plot_wireframe(x,y,z, cmap = cm.coolwarm, linewidth = 1)# Wireframe doesn't work properly
+    wf.plot_surface(x,y,z, cmap = cm.coolwarm, linewidth = 1)# Make the surface plot instead with a colour map and still some wireframe
+    wf.view_init(60, -120)# Its reversed from matplotlib
+    
+    pyplot.show()
 
 
 #"""
 households = Households()
-print(households.data.head(100))
-print(households.consumption(1400, 0))
+#print(households.data.head(100))
+print(households.data)
+#print(households.consumption(1400, 300))
+make_3Dfunction_plot(households.consumption)
 #"""
