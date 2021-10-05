b1419aa9f61f6952d9ea643e90bfbf0d8776d705
Tim Heiszwolf <32781879+TimHeiszwolf@users.noreply.github.com>
2021-10-06 00:31:43 +0200

    FIX Storage
    Fixed (hopefully ) all the bugs in Storage.py. Of course still needs cleaning but now is fully functional.

--- Controller.py
+++ Controller.py
@@ -91,7 +91,7 @@ class Controller:
         self.data["Time"].append(self.time)
         self.data["Supply"].append(self.supply.output(self.time))# Inefficient that we call these functions 3 times (here twice and once in controller).
         self.data["Demand"].append(self.demand.consumption(self.time))
-        self.data["Storage"].append(self.train_track.power)
+        self.data["Storage"].append(self.train_track.get_power())# power)
         self.data["Difference"].append(self.demand.consumption(self.time) - self.supply.output(self.time) + self.train_track.power)
         self.data["Losses"].append(sum(self.train_track.losses.values()))
         
--- Storage.py
+++ Storage.py
@@ -4,13 +4,13 @@ import datetime
 import math
 
 class TrainTrack:
-    def __init__(self, carts = 100, track_dimensions = [20000, 2000], mass_per_cart = 67.5 * 2170):
+    def __init__(self, carts = 100, track_dimensions = [20000, 2000], mass_per_cart = 67.5 * 2170, minimal_distance = 100):
         
         # TODO make it so that the carts get placed in their relevant place instead of being put by default on top.
         self.carts_on_track = []# Each cart on the track will be reprisented by a number which indicates its position on the track.
         self.velocity = 0# The velocity of the track.
         self.net_force = 0
-        self.carts_of_track = {"Bottom" : [], "Top" : carts}# All the carts by default start at the top.
+        self.carts_of_track = {"Bottom" : 0, "Top" : carts}# All the carts by default start at the top.
         
         self.track_length = track_dimensions[0]
         self.track_height = track_dimensions[1]
@@ -19,18 +19,14 @@ class TrainTrack:
         self.mass_per_cart = mass_per_cart
         
         
-        self.power = 0
         
-        
-        self.minimal_distance = 100
+        self.minimal_distance = minimal_distance
         
         
         
         self.force_of_generator = 0
+        self.efficiency_generator = 0.6# TODO IMPLIMENT SOMEHWERE
         self.other_power = 0
-        
-        
-        
         
         
         self.losses = {"Friction" : 0, "Efficiency" : 0}
@@ -39,14 +35,14 @@ class TrainTrack:
         # Todo add proper friction https://www.sciencedirect.com/science/article/pii/S0043164814003718
         g = 9.81
         
-        drag_coefficient = 1.05 * (2.591 * 2.438)
-        friction_coefficient = 0.01# Uit duim gezogen
+        drag_coefficient = 1.05 * (2.591 * 2.438) / 2
+        friction_coefficient = 0.001# Uit duim gezogen
         
         if velocity == "NaN":
             velocity = self.velocity
         
         
-        return - len(self.carts_of_track) * np.sign(velocity) * (density_air * velocity**2 * drag_coefficient / 2 + abs(velocity) * np.cos(self.angle) * g * self.mass_per_cart * friction_coefficient)
+        return -np.sign(velocity) *  len(self.carts_of_track) * (density_air * velocity**2 * drag_coefficient + np.cos(self.angle) * g * self.mass_per_cart * friction_coefficient)
     
     
     def get_gravity(self):
@@ -119,15 +115,20 @@ class TrainTrack:
             self.carts_on_track.pop(index)
             self.carts_of_track[location] = self.carts_of_track[location] + 1
         
-        self.other_power = self.other_power - energy_left_over / delta_time
-        
+        self.other_power = self.other_power - energy_left_over * 6 / (delta_time * np.pi**2)# TODO: maybe turn this into a power series of 1/x^2 from 1 to inf (is equal to pi^2/6) # Sum[(100/(\[Pi]^2/6))/x^2, {x, 1, \[Infinity]}]
+    
+    def get_power(self):
+        return self.other_power + self.force_of_generator * self.velocity
+    
     
     def do_tick(self, delta_time):
         
-        self.power = self.other_power + self.force_of_generator * self.velocity# Set the power
-        self.other_power = 0
         
-        self.losses["Friction"] = abs(self.get_friction()) * self.velocity
+        
+        self.losses["Friction"] = abs(self.get_friction()) * self.velocity * delta_time
+        self.losses["Efficiency"] = (1 - self.efficiency_generator) * abs(self.get_power())#TODO: impliment such that it switches efficiency based on wheater it is postive or negative energy generation
+        self.other_power = self.other_power / 2# TODO: properly explain this power series of 1/x^2 from 1 to inf (is equal to pi^2/6) # Sum[(100/(\[Pi]^2/6))/x^2, {x, 1, \[Infinity]}]
+        
         
         if len(self.carts_on_track) == 0:# Prevent devide by zero error
             acceleration = 0
@@ -138,8 +139,82 @@ class TrainTrack:
         change_in_position = self.velocity * delta_time + 0.5 * acceleration * delta_time**2
         self.carts_on_track = [i + change_in_position for i in self.carts_on_track]# Change the position of each of the carts.
         self.velocity = self.velocity + acceleration * delta_time
-        
+    
+    def return_data(self):
+        return {"Positions": np.round(self.carts_on_track, 2), "Velocity" : np.round(self.velocity, 2), "Forces (gravity, friction, generator)" : [np.round(self.get_gravity()), np.round(self.get_friction()), np.round(self.force_of_generator)], "Power" : np.round(self.get_power()), "Other carts" : self.carts_of_track}
         
 
+"""
+train_track = TrainTrack()
+time = 0
+train_track.carts_on_track.append(19000)
 
+for i in range(10):
+    print(time, train_track.return_data())
+    train_track.do_tick(1)
+    time = time + 1
+
+print("\n Adding a generator force. \n")
+train_track.force_of_generator = -(train_track.get_gravity() + train_track.get_friction())#142978
+
+for i in range(20):
+    print(time, train_track.return_data())
+    train_track.do_tick(1)
+    time = time + 1
+
+print("\n Adding another cart. \n")
+train_track.add_cart("Top")
+#train_track.force_of_generator = -(train_track.get_gravity() + train_track.get_friction())
+
+for i in range(20):
+    print(time, train_track.return_data())
+    train_track.do_tick(1)
+    time = time + 1
+
+print("\n Now trying to decelerate. \n")
+train_track.force_of_generator = -1.4 * (train_track.get_gravity() + train_track.get_friction())#142978
+
+for i in range(20):
+    print(time, train_track.return_data())
+    train_track.do_tick(1)
+    time = time + 1
+
+print("\n Running until at the bottum. \n")
+train_track.force_of_generator = - (train_track.get_gravity() + train_track.get_friction())
+
+print(time, train_track.return_data())
+for i in range(2000):
+    #print(time, train_track.return_data())
+    train_track.do_tick(1)
+    time = time + 1
+print(time, train_track.return_data())
+
+print("\n Removing a cart. \n")
+train_track.remove_cart(1, 0)
+train_track.force_of_generator = - (train_track.get_gravity() + train_track.get_friction())
+
+for i in range(20):
+    print(time, train_track.return_data())
+    train_track.do_tick(1)
+    time = time + 1
+
+print("\n Adding another cart. \n")
+train_track.add_cart("Top")
+train_track.force_of_generator = -(train_track.get_gravity() + train_track.get_friction())
+
+for i in range(20):
+    print(time, train_track.return_data())
+    train_track.do_tick(1)
+    time = time + 1
+
+print("\n Removing and adding a cart. \n")
+train_track.remove_cart(1, 0)
+train_track.add_cart("Top")
+
+for i in range(20):
+    print(time, train_track.return_data())
+    train_track.do_tick(1)
+    time = time + 1
+
+#"""
 
