52c515c69a93b6cee8c071242c5ade8c6053ebf7
Tim Heiszwolf <32781879+TimHeiszwolf@users.noreply.github.com>
2021-10-14 16:52:09 +0200

    Update Simulation.py

--- Simulation.py
+++ Simulation.py
@@ -17,43 +17,44 @@ from Storage import TrainTrack
 from Controller import Controller
 
 
-train_track = TrainTrack(carts = 200)
+days_to_simulate = 364
+
+
+
+
+
+train_track = TrainTrack(carts = 500000)
 supply = WindSupply(amount_of_windmills = 60)#WindSupplyDummy(100000)
-demand = Households(amount_of_households_per_type = [100000, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+demand = Households(amount_of_households_per_type = [125000, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-controller = Controller(train_track = train_track, supply = supply, demand = demand, delta_time = 30)
+controller = Controller(train_track = train_track, supply = supply, demand = demand, delta_time = 10)
 
-make_3Dfunction_plot(controller.simulate, amount_of_days = 365)#controller.simulate(2*24*3600)
+make_3Dfunction_plot(controller.get_difference_supply_demand, amount_of_days = days_to_simulate, zlabel = "Difference in power (Watts)")
+make_3Dfunction_plot(controller.get_sastisfaction_supply_demand, amount_of_days = days_to_simulate, zlabel = "Satisfaction")
+make_3Dfunction_plot(controller.simulate, amount_of_days = days_to_simulate, zlabel = "Difference in power (Watts)")#controller.simulate(days_to_simulate*24*3600)
 
 data = pd.DataFrame(controller.data)
-
-data["Satisfaction"] =  ((data["Supply"] - data["Storage"]) / data["Demand"])
 print(data)
  
-
 plt.plot(data["Time"] / (3600 * 24), data["Supply"], label="Supply")
 plt.plot(data["Time"] / (3600 * 24), data["Demand"], label="Demand")
-#plt.title("Delhi Temperature")
 plt.xlabel("Time (days)")
 plt.ylabel("Supply (Watts)")
 plt.legend(loc="upper left")
 plt.show()
 
 plt.plot(data["Time"] / (3600 * 24), data["Satisfaction"])
-#plt.title("Delhi Temperature")
 plt.xlabel("Time (days)")
 plt.ylabel("Satisfaction")
 plt.show()
 
 plt.plot(data["Time"] / (3600 * 24), data["Velocity"])
-#plt.title("Delhi Temperature")
 plt.xlabel("Time (days)")
 plt.ylabel("Velocity on track")
 plt.show()
 
 plt.plot(data["Time"] / (3600 * 24), data["Amount carts on top"], label = "Amount of carts at top")
 plt.plot(data["Time"] / (3600 * 24), data["Amount carts on bottom"], label = "Amount of carts at bottom")
-#plt.title("Delhi Temperature")
 plt.xlabel("Time (days)")
 plt.ylabel("Amount of carts")
 plt.legend(loc="upper left")
