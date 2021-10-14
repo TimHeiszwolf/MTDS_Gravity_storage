a4e654174ac3ae86b12df0b35182d13b4d233341
Tim Heiszwolf <32781879+TimHeiszwolf@users.noreply.github.com>
2021-10-14 15:23:58 +0200

    ADD working controller

--- Controller.py
+++ Controller.py
@@ -5,6 +5,7 @@ import math
 
 from mpl_toolkits.mplot3d import axes3d
 from matplotlib import pyplot
+import matplotlib.pyplot as plt
 from matplotlib import cm
 
 from Helpers import make_3Dfunction_plot
@@ -16,7 +17,7 @@ from Storage import TrainTrack
 
 
 class Controller:
-    def __init__(self, train_track = TrainTrack(), supply = WindSupplyDummy(1000), demand = HouseholdsDummy(1000), delta_time = 0.1):
+    def __init__(self, train_track = TrainTrack(), supply = WindSupplyDummy(1000), demand = HouseholdsDummy(1000), delta_time = 5):
         
         self.time = 0
         self.delta_time = delta_time
@@ -25,7 +26,7 @@ class Controller:
         self.demand = demand
         self.train_track = train_track
         
-        self.data = {"Time": [], "Supply" : [], "Demand" : [], "Storage" : [], "Difference" : [], "Losses": []}
+        self.data = {"Time" : [], "Velocity" : [], "Amount carts on track" : [], "Supply" : [], "Demand" : [], "Storage" : [], "Difference" : [], "Losses": [], "Amount carts on top" : [], "Amount carts on bottom" : []}
         
         
         
@@ -38,6 +39,7 @@ class Controller:
         """
         A function which checks if the carts are still on/near the track and if not removes them from circulation. If allowed it also tries to add a new cart.
         """
+        
         train_track = self.train_track# So that we don't have to put self in front of it each time.
         
         did_thing = False
@@ -68,82 +70,48 @@ class Controller:
             return False
     
     def controller(self):
+        """"
+        This is the actual part of the controller that controlls how and when carts are added and also determines the force the generator will use. Speciclicly this controller always tried to add a cart and does the controll for power purley based on (steady-state) speed by adjusting the generator force. See the report for more details.
+        """
         
-        train_track = self.train_track# So that we don't have to put self in front of it each time.
+        train_track = self.train_track# So that we don't have to put self. in front of it each time.
         
-        while self.check_carts():
+        net_demand = self.supply.output(self.time) - self.demand.consumption(self.time)# Calculate the difference in supply and demand that the storage system should fill.
+        
+        while self.check_carts():# Check the carts until no more changes are done.
             continue
         
-        train_track.add_cart()
+        if len(train_track.carts_on_track) > 0:# If there are other carts on the track let the track itself decide were (and if it is possible) to add a cart.
+            train_track.add_cart()
+        else:# If there are no other carts on the track the velocity will be zero and thus the controller will have to determine where to add the carts.
+            if net_demand > 0:
+                train_track.add_cart("Bottom")
+            elif net_demand < 0:
+                train_track.add_cart("Top")
         
-        net_demand = self.supply.output(self.time) - self.demand.consumption(self.time)
-        neutral_force = -(train_track.get_gravity() + train_track.get_friction())
-        #print(neutral_force)
-        neutral_power = train_track.get_efficiency_generator() * abs(train_track.get_gravity() + train_track.get_friction()) * train_track.velocity
+        neutral_force = -(train_track.get_gravity() + train_track.get_friction())# Calculate the force the generator needs to have to maintain the current speed of the carts.
+        neutral_power = train_track.get_efficiency_generator() * abs(train_track.get_gravity() + train_track.get_friction()) * train_track.velocity# Calculate the assosiated power out/input.
         
-        if neutral_force != 0:
-            needed_change_in_speed = -(neutral_power - net_demand) / abs(neutral_force)
+        if neutral_force != 0:# Prevent a devide by zero error. Will only be zero if there are no carts (or if the carts are at terminal velocity which should never happen).
+            needed_change_in_speed = -(neutral_power - net_demand) / abs(neutral_force)# Calculate the change in speed needed to get to the desired power output. It ignores a potential increase in friction but that is not a problem since during the next tick it will take into account an increased friction and thus it will exponentially decrease any effect this has.
         else:
             needed_change_in_speed = 0
         
-        if train_track.velocity > self.max_speed:
+        if train_track.velocity > self.max_speed:# If the speed is maximum then set the change in velocity to be zero.
             needed_change_in_speed = 0#train_track.velocity - self.max_speed
         elif train_track.velocity < -self.max_speed:
             needed_change_in_speed = 0#self.max_speed + train_track.velocity
         
-        #print(needed_change_in_speed)
-        acceleration = min([max([needed_change_in_speed / self.delta_time, -1]), 1])
-        train_track.force_of_generator = neutral_force + acceleration * train_track.mass_per_cart * len(train_track.carts_on_track)
+        acceleration = min([max([needed_change_in_speed / self.delta_time, -self.max_acceleration]), self.max_acceleration])# Make sure the maximum acceleration also is not exeded.
+        train_track.force_of_generator = neutral_force + acceleration * train_track.mass_per_cart * len(train_track.carts_on_track)# The force is calculated based on the neutral force plus any acceleration needed.
         
-        if train_track.force_of_generator <=0:
+        if train_track.force_of_generator <=0:# Make sure the force is not negative.
             train_track.force_of_generator = 0
         
-        
-        
-        """
-        net_demand = self.supply.output(self.time) - self.demand.consumption(self.time)
-        neutral_force = train_track.get_gravity() + train_track.get_friction()
-        neutral_power = abs(train_track.get_gravity() + train_track.get_friction()) * self.train_track.velocity
-        
-        if neutral_force != 0:
-            needed_change_in_speed = (neutral_power - net_demand) / abs(neutral_force)# Calculates the change in speed needed to match the needed demand. Thus ignores inital loss of power due to acceleration. Ignores any change in friction.
-        else:
-            needed_change_in_speed = 2 * self.max_acceleration * self.delta_time * np.sign(neutral_power - net_demand)
-        
-        needed_acceleration = needed_change_in_speed / self.delta_time
-        acceleration = max([-self.max_acceleration, min([self.max_acceleration, needed_acceleration])])# Makes sure the acceleration is within limits.
-        train_track.force_of_generator = max([-neutral_force + acceleration * train_track.mass_per_cart * len(train_track.carts_on_track), 0])
-        """
-        
-        
-        
-        """
-        neutral_force = 
-        
-        neutral_power = abs(neutral_force) * self.train_track.velocity# Power is defined as negative when flowing out and postive when flowing in.
-        net_demand = self.supply.output(self.time) - self.demand.consumption(self.time)
-        
-        if neutral_force != 0:
-            needed_change_in_speed = (neutral_power - net_demand) / abs(neutral_force)# Calculates the change in speed needed to match the needed demand. Thus ignores inital loss of power due to acceleration. Ignores any change in friction.
-        else:
-            needed_change_in_speed = 2 * self.max_acceleration * self.delta_time * np.sign(neutral_power - net_demand)
-        
-        if abs(needed_change_in_speed + train_track.velocity) >= self.max_speed:# If the speed becomes larger than the allowed speed then (try to) add carts. Also set the needed_change_in_speed to the change needed to reach the maximum.
-            if train_track.velocity > 0:
-                train_track.add_cart("Bottom")
-                needed_change_in_speed = self.max_speed - train_track.velocity
-            elif train_track.velocity <=0:
-                train_track.add_cart("Top")
-                needed_change_in_speed = -self.max_speed - train_track.velocity
-        
-        needed_acceleration = needed_change_in_speed / self.delta_time
-        acceleration = max([-self.max_acceleration, min([self.max_acceleration, needed_acceleration])])# Makes sure the acceleration is within limits.
-        train_track.force_of_generator = max([-neutral_force + acceleration * train_track.mass_per_cart * len(train_track.carts_on_track), 0])
-        
-        #print(train_track.force_of_generator)
-        """
-    
     def get_debug_print(self):
+        """
+        A function which can be used to print stuff for debugging purposes.
+        """
         
         print("")
         print("Time:", self.time)
@@ -157,27 +125,37 @@ class Controller:
     
     def do_tick(self):
         """
-        TO WRITE
+        This sub-function does a time-step (tick as it is called). During each tick it saves the data to the data dictionairy and then exicutes the controller and lets the train traick also do a tick.
         """
         
         self.data["Time"].append(self.time)
+        self.data["Velocity"].append(self.train_track.velocity)
+        self.data["Amount carts on track"].append(len(self.train_track.carts_on_track))
         self.data["Supply"].append(self.supply.output(self.time))# Inefficient that we call these functions 3 times (here twice and once in controller).
         self.data["Demand"].append(self.demand.consumption(self.time))
-        self.data["Storage"].append(self.train_track.get_power())# power)
+        self.data["Storage"].append(self.train_track.get_power())
         self.data["Difference"].append(self.demand.consumption(self.time) - self.supply.output(self.time) + self.train_track.get_power())
         self.data["Losses"].append(sum(self.train_track.losses.values()))
+        self.data["Amount carts on top"].append(self.train_track.carts_of_track["Top"])
+        self.data["Amount carts on bottom"].append(self.train_track.carts_of_track["Bottom"])
         
-        self.controller()
-        self.train_track.do_tick(self.delta_time)
+        self.controller()# Let the controller do its thing.
+        self.train_track.do_tick(self.delta_time)# Do a tick for the train track (the actual physics).
         
         self.time = self.time + self.delta_time
     
-    def simulate(self, end_time = 364 * 24 * 3600):
+    def simulate(self, end_time_seconds = 364 * 24 * 3600, end_time_days = 0):
+        """
+        This sub-function does the simulation until a certain end time. It also returns some data so it can be used in the make_3Dfunction_plot.
+        """
+        
+        end_time = end_time_seconds + 3600 * 24 * end_time_days
         
         while self.time <= end_time:
             self.do_tick()
-            #print(self.time / (3600 * 24))
-            print(self.get_debug_print())
+            #print(self.get_debug_print())
+        #print(self.get_debug_print())
+        return self.demand.consumption(self.time) - self.supply.output(self.time) + self.train_track.get_power()
 
 """
 households = Households()
@@ -188,10 +166,10 @@ def get_difference(time_seconds, time_days = 0, output = households, inputt  = w
 
 make_3Dfunction_plot(get_difference)
 #"""
-#"""
+"""
 controller = Controller()
 
-controller.simulate(20)
+controller.simulate(200)
 
 data = pd.DataFrame(controller.data)
 
